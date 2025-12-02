b658f0e6a0e110c396d7e0a15b0e2bf796abfe7c
mulikruchi07 <mulikruchi07@gmail.com>
2025-12-03 04:27:42 +0530

    removed example site

--- platforms/example_site/locators.py
+++
@@ -1,6 +0,0 @@
-# example template fro platforms
-LOGIN_BUTTON = "//a[contains(., 'Sign in') or contains(., 'Login')]" # GitHub uses 'Sign in'
-USERNAME = "//input[@id='login_field']"
-PASSWORD = "//input[@id='password']"
-SEARCH_BOX = "//input[@placeholder='Search GitHub']"
-SEARCH_BUTTON = "//button[contains(., 'Search') or @type='submit']"
