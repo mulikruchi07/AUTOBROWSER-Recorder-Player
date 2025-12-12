13c6e80b84daf653aa4f218ff90e241caed2aa60
Ruchi Mulik <mulikruchi07@gmail.com>
2025-12-13 00:31:35 +0530

    exception

--- core/driver_manager.py
+++ core/driver_manager.py
@@ -6,15 +6,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver_with_temp_profile(headless=False):
     profile_dir = tempfile.mkdtemp(prefix="autobrowser-")
+
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument(f"--user-data-dir={profile_dir}")
-    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-popup-blocking")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--disable-popup-blocking")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
+
     if headless:
         options.add_argument("--headless=new")
         options.add_argument("--window-size=1920,1080")
@@ -22,11 +21,12 @@ def get_driver_with_temp_profile(headless=False):
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
 
+    # remove webdriver flag
     try:
         driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-            "source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+            "source": "Object.defineProperty(navigator,'webdriver',{get:()=>undefined});"
         })
-    except Exception:
+    except:
         pass
 
     return driver
